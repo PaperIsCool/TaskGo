9b856008d677266a4dc38f5b21ce8033a08daccc
Neer Grover <PaperIsCool5@outlook.com>
2025-03-05 19:19:18 +0300

    Removed GH pages support

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -14,7 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production'
-    ? '/TaskGo/'  // Replace with your GitHub repository name
-    : '/',
 })
